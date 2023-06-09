b77942fa0389c1ecbca7b19de0571d3a0d314b58
tavice <thomasavice.ta@gmail.com>
2023-06-08 19:30:46 -0700

    changed route with traditional react17 reactdom.render

--- src/index.js
+++ src/index.js
@@ -5,14 +5,15 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Router>
-    <React.StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
       <App />
-    </React.StrictMode>
-  </Router>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
