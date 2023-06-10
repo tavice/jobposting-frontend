e4e72b67c65fc91489031f6203d7d831c43726e9
tavice <thomasavice.ta@gmail.com>
2023-06-10 11:39:05 -0700

    added jobfilter

--- src/pages/EmployerList.js
+++ src/pages/EmployerList.js
@@ -2,39 +2,40 @@ import { useEffect, useState, useCallback } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Container from "@mui/material/Container";
-import {  Typography, Grid, Paper, Divider, CircularProgress } from "@mui/material";
+import {
+  Typography,
+  Grid,
+  Paper,
+  Divider,
+  CircularProgress,
+} from "@mui/material";
 import Paid from "@mui/icons-material/Paid";
 import LocationOn from "@mui/icons-material/LocationOn";
 import LanguageIcon from "@mui/icons-material/Language";
 import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
-import PsychologyIcon from '@mui/icons-material/Psychology';
+import PsychologyIcon from "@mui/icons-material/Psychology";
 
 const EmployerList = ({ baseUrl }) => {
   const [employerList, setEmployerList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [filter, setFilter] = useState("");
+  const [jobListings, setJobListings] = useState({});
 
-  //=======================================================//
-  // Fetching Employer List//
-  const fetchEmployerList = useCallback( async () => {
-    const response = await axios.get(`${baseUrl}/api/employers/?format=json`);
-    const data = response.data;
-    setEmployerList(data);
+  const fetchEmployerList = useCallback(async () => {
+    try {
+      const response = await axios.get(`${baseUrl}/api/employers/?format=json`);
+      const data = response.data;
+      setEmployerList(data);
+    } catch (error) {
+      console.log(error);
+    }
   }, [baseUrl]);
 
-  useEffect(() => {
-    fetchEmployerList();
-  }, [fetchEmployerList]);
-
-  console.log(employerList);
-  //=======================================================//
-  // Fetching Job Listings//
-  const fetchJobListings = useCallback( async () => {
+  const fetchJobListings = useCallback(async () => {
     try {
       const response = await axios.get(`${baseUrl}/api/joblistings`);
-
       const data = response.data;
-      console.log(data);
       return data;
     } catch (error) {
       console.log(error);
@@ -42,179 +43,172 @@ const EmployerList = ({ baseUrl }) => {
     }
   }, [baseUrl]);
 
-  //=======================================================//
-  // once we have the job listings, we need to group them by employer
-  // that means we need to create a new object where the keys are the employer ids
-  // and the values are the job listings for that employer
+  useEffect(() => {
+    fetchEmployerList();
+  }, [fetchEmployerList]);
 
-  const [jobListings, setJobListings] = useState({});
   useEffect(() => {
     const getJobListings = async () => {
-      const data = await fetchJobListings(); // fetch job listings
+      const data = await fetchJobListings();
       const jobListings = data.reduce((acc, job) => {
-        // group job listings by employer
         if (!acc[job.employer]) {
-          // if the employer doesn't exist in the object yet, add it
-          acc[job.employer] = []; // initialize the array
+          acc[job.employer] = [];
         }
-        acc[job.employer].push(job); // add the job listing to the array
+        acc[job.employer].push(job);
         return acc;
       }, {});
       setJobListings(jobListings);
       setIsLoading(false);
-      
     };
     getJobListings();
   }, [fetchJobListings]);
-
-  console.log("joblisting is ", jobListings);
-
-  //=======================================================//
-  // Filtering Job Listings//
-
-  const [filter, setFilter] = useState("");
-  //const [filteredJobListings, setFilteredJobListings] = useState([]);
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value);
   };
 
-  // const handleFilterSubmit = (event) => {
-  //   event.preventDefault();
-  //   const filteredJobListings = Object.values(jobListings)
-  //     .flatMap((jobs) => jobs)
-  //     .filter((job) =>
-  //       job.jobtitle.toLowerCase().includes(filter.toLowerCase())
-  //     );
-  //   console.log("filtered joblisting is ", filteredJobListings);
-  //   setFilteredJobListings(filteredJobListings);
-  // };
-
-  // console.log("filtered joblisting is ", filteredJobListings);
-
-  // console.log("filter is ", filter);
-
-
-
-
-  //truncated job description
-
-
-  //=======================================================//
-  // Rendering Employer List//
+  const filteredJobListings = Object.values(jobListings)
+    .flat()
+    .filter((jobListing) =>
+      jobListing.jobtitle.toLowerCase().includes(filter.toLowerCase())
+    );
 
   return (
     <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-       {isLoading ? (
+      {isLoading ? (
         <CircularProgress />
       ) : (
         <>
-      <Typography
-        variant="h2"
-        style={{ marginBottom: 20, alignItems: "center" }}
-      >
-        Current Jobs Available
-      </Typography>
-      <div className="filter-container">
-        <label htmlFor="filter">Search Job Title:</label>
-        <input
-          type="text"
-          id="filter"
-          value={filter}
-          onChange={handleFilterChange}
-        />
-      </div>
-      {employerList.map((employer) => (
-        <Grid
-          container
-          spacing={2}
-          style={{ alignItems: "center", margin: 0 }}
-          key={employer.id}
-        >
-          <Paper
-            elevation={3}
-            style={{
-              width: "80%",
-              alignItems: "center",
-              padding: 20,
-              margin: " auto",
-              marginBottom: 40,
-            }}
+          <Typography
+            variant="h2"
+            style={{ marginBottom: 20, alignItems: "center" }}
           >
-            <Grid
-              item
-              xs={12}
-              sm
-              container
-              style={{
-                justifyContent: "space-between",
-                marginBottom: 20,
-                marginTop: 20,
-              }}
-            >
-              <Typography variant="h3" gutterBottom>
-                {employer.companyname}
-              </Typography>
-              <div style={{ justifyContent: "center" }}>
-                <img src={employer.logo} alt="Company Logo" height={100} />
-              </div>
-              <Typography variant="body1" gutterBottom>
-                <LanguageIcon />{" "}
-                <a
-                  href={employer.website}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {employer.website}
-                </a>
-              </Typography>
-              <Typography variant="body1">
-                <LocationOn /> {employer.location}{" "}
-              </Typography>
-            </Grid>
-            <Divider style={{ height: 5 }} />
-            <Typography
-              variant="h4"
-              style={{ marginBottom: 40, marginTop: 40 }}
-            >
-              Open Roles:{" "}
-            </Typography>
-            {jobListings[employer.id] && jobListings[employer.id].length > 0 ? (
-              jobListings[employer.id].map((job) => (
+            Current Jobs Available
+          </Typography>
+          <div className="filter-container">
+            <label htmlFor="filter">Search Job Title:</label>
+            <input
+              type="text"
+              id="filter"
+              value={filter}
+              onChange={handleFilterChange}
+            />
+          </div>
+          {filteredJobListings.length > 0 ? (
+            filteredJobListings.map((jobListing) => (
+              <Grid
+                container
+                spacing={2}
+                style={{ alignItems: "center", margin: 0 }}
+                key={jobListing.id}
+              >
                 <Paper
                   elevation={3}
                   style={{
                     width: "80%",
                     alignItems: "center",
                     padding: 20,
-                    margin: "auto",
-                    marginBottom: 20,
+                    margin: " auto",
+                    marginBottom: 40,
                   }}
-                  key={job.id}
                 >
-                  <Typography variant="h5" style={{ marginBottom: 20 }}>
-                    <Link to={`/Job Listings/${job.id}`}>{job.jobtitle}</Link>
+                  <Grid
+                    item
+                    xs={12}
+                    sm
+                    container
+                    style={{
+                      justifyContent: "space-between",
+                      marginBottom: 20,
+                      marginTop: 20,
+                    }}
+                  >
+                    <Typography variant="h3" gutterBottom>
+                      {jobListing.companyname}
+                    </Typography>
+                    <div style={{ justifyContent: "center" }}>
+                      <img
+                        src={jobListing.logo}
+                        alt="Company Logo"
+                        height={100}
+                      />
+                    </div>
+                    <Typography variant="body1" gutterBottom>
+                      <LanguageIcon />{" "}
+                      <a
+                        href={jobListing.website}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {jobListing.website}
+                      </a>
+                    </Typography>
+                    <Typography variant="body1">
+                      <LocationOn /> {jobListing.location}{" "}
+                    </Typography>
+                  </Grid>
+                  <Divider style={{ height: 5 }} />
+                  <Typography
+                    variant="h4"
+                    style={{ marginBottom: 40, marginTop: 40 }}
+                  >
+                    Open Roles:{" "}
                   </Typography>
+                  {jobListings[jobListing.id] &&
+                  jobListings[jobListing.id].length > 0 ? (
+                    jobListings[jobListing.id].map((job) => (
+                      <Paper
+                        elevation={3}
+                        style={{
+                          width: "80%",
+                          alignItems: "center",
+                          padding: 20,
+                          margin: "auto",
+                          marginBottom: 20,
+                        }}
+                        key={job.id}
+                      >
+                        <Typography
+                          variant="h5"
+                          style={{ marginBottom: 20 }}
+                        >
+                          <Link to={`/Job Listings/${job.id}`}>
+                            {job.jobtitle}
+                          </Link>
+                        </Typography>
 
-                  <Stack direction="row" useFlexGap flexWrap="wrap" spacing={1} style={{padding:20}}>
-                    <Chip icon={<LocationOn />} label={job.location} />
-                    <Chip icon={<Paid />} label={job.salary} />
-                    <Chip icon={<PsychologyIcon />} label={job.jobrequirements} />
-                  
-                  </Stack>
+                        <Stack
+                          direction="row"
+                          useFlexGap
+                          flexWrap="wrap"
+                          spacing={1}
+                          style={{ padding: 20 }}
+                        >
+                          <Chip icon={<LocationOn />} label={job.location} />
+                          <Chip icon={<Paid />} label={job.salary} />
+                          <Chip
+                            icon={<PsychologyIcon />}
+                            label={job.jobrequirements}
+                          />
+                        </Stack>
 
-                  <Typography variant="body1">
-                    <strong>Description:</strong> {job.description}
-                  </Typography>
+                        <Typography variant="body1">
+                          <strong>Description:</strong> {job.description}
+                        </Typography>
+                      </Paper>
+                    ))
+                  ) : (
+                    <Typography variant="body1">
+                      No job listings found.
+                    </Typography>
+                  )}
                 </Paper>
-              ))
-            ) : (
-              <Typography variant="body1">No job listings found.</Typography>
-            )}
-          </Paper>
-        </Grid>
-      ))}
-      </>
+              </Grid>
+            ))
+          ) : (
+            <Typography variant="body1">No job listings found.</Typography>
+          )}
+        </>
       )}
     </Container>
   );
